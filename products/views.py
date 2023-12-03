eb834eada134e60566cd2d21c34840e6ea847800
Raymond Bautista <mynameisrhay@gmail.com>
2023-12-03 20:52:29 +0800

    Rendered the template. Fetched data from the db using the render function and providing 3 arguments (request, template (index.html), and a dictionary {'products': products}. Fetched data by using render tags and doing for loop to get data from the database.

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
-    return HttpResponse("Hello World")
+    return render(request, 'index.html', {'products': products})
 
 
 def new(request):
